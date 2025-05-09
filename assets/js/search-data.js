1ca3d1e508c296de50f5166d00fb3b1fceea4d0c
ottersem <58806533+ottersem@users.noreply.github.com>
2025-05-09 15:18:29 +0000

    Deploying to gh-pages from @ ottersem/ottersem.github.io@704b96be689b0ad12a685c572dabc66c84ec0ee1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,15 +37,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-image-formation",
+        },{id: "post-geometric-primitives-and-transformations",
       
-        title: "Image Formation",
+        title: "Geometric primitives and transformations",
       
       description: "Image Formation from CVAA",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/image-formation/";
+          window.location.href = "/blog/2025/GPaT/";
         
       },
     },{id: "post-linear-algebra",
